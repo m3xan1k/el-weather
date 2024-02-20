2edbaf6b8a5dc2c80168a6c712a0f51b3172fd92
m3xan1k <m3xan1k@duck.com>
2024-02-20 14:02:24 +0300

    forecast wip

--- weather.el
+++ weather.el
@@ -3,6 +3,18 @@
 
 (defvar GEO-URL "http://www.geoplugin.com/")
 (defvar LOCATION-URL "http://www.geoplugin.net/json.gp")
+(defvar FORECAST-URL "https://api.open-meteo.com/v1/forecast")
+
+
+
+(defvar output-fields
+  '(temperature_2m
+    relative_humidity_2m
+    rain
+    showers
+    snowfall
+    cloud_cover
+    wind_speed_10m))
 
 (defun get-geo-html ()
   (request-response-data
@@ -16,6 +28,18 @@
      :parser 'json-read
      :sync t)))
 
+;; ;; https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,rain,showers,snowfall,cloud_cover,wind_speed_10m&wind_speed_unit=ms&forecast_days=1
+
+(defun get-forecast-data (lat lon tz)
+  (request-response-data
+   (request FORECAST-URL
+     :params '(("latitude" . lat)
+	       ("longitude" . lon)
+	       ("timezone" . tz)
+	       ("wind_speed_unit" . "ms")
+	       ("forecast_days" . 1)
+	       ("current" . (mapconcat 'symbol-name output-fields ","))))))
+
 (defun get-ext-ip ()
   (let* ((html (with-temp-buffer
 		 (insert (get-geo-html))
@@ -23,11 +47,15 @@
 	 (input (esxml-query "input#entered_ip" html)))
     (cdr (assoc 'value (cadr input)))))
 
+
+
 (defun main ()
   (let* ((ip (get-ext-ip))
 	 (location-data (get-location-data ip))
 	 (lat (cdr (assoc 'geoplugin_latitude location-data)))
-	 (lon (cdr (assoc 'geoplugin_longitude location-data))))
+	 (lon (cdr (assoc 'geoplugin_longitude location-data)))
+	 (tz (cdr (assoc 'geoplugin_timezone location-data)))
+	 (forecast-data (get-forecast-data lat lon tz)))
+    forecast-data
     ))
 
-(main)
